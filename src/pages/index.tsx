dcc18c8adc08e3c25375edbef148d6ecd2917395
Connor <edmchzty@gmail.com>
2026-04-01 23:10:26 -0400

    Redirect homepage to documentation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,6 @@
 import type {ReactNode} from 'react';
-import Link from '@docusaurus/Link';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import Heading from '@theme/Heading';
-
-import styles from './index.module.css';
+import {Redirect} from '@docusaurus/router';
 
 export default function Home(): ReactNode {
-  const {siteConfig} = useDocusaurusContext();
-  const logo = useBaseUrl('/img/logo.svg');
-
-  return (
-    <Layout
-      title={siteConfig.title}
-      description="Capstone documentation site for the Chemware Engineering industrial plant modeling project.">
-      <main className={styles.page}>
-        <section className={styles.hero}>
-          <div className="container">
-            <div className={styles.heroInner}>
-              <img className={styles.logo} src={logo} alt="Chemware Docs icon" />
-              <Heading as="h1" className={styles.title}>
-                Chemware Docs
-              </Heading>
-              <Link className={styles.cta} to="/docs/intro">
-                Documentation
-              </Link>
-            </div>
-          </div>
-        </section>
-      </main>
-    </Layout>
-  );
+  return <Redirect to="/docs/intro" />;
 }
